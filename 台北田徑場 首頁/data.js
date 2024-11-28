d77d2c285b05abcdb4bc6239f5910e01a24b91b8
Ian970923 <ianlai0923@gmail.com>
2024-11-28 11:58:13 +0800

    台北田徑場

---
+++ 台北田徑場 首頁/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "跑道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1345.5,
+      "initialViewParameters": {
+        "yaw": 2.3671092700444927,
+        "pitch": 0.053429481164968706,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "台北田徑場",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
