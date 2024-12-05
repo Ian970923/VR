3bafaaef1d7c6794875572811a261ebc08522a01
Ian970923 <ianlai0923@gmail.com>
2024-12-05 10:34:55 +0800

    田徑場

---
+++ 觀眾席/data.js
@@ -0,0 +1,262 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "草地",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.33269249743377927,
+        "pitch": 0.02300478787984339,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9171607605720906,
+          "pitch": 0.04400810831602264,
+          "rotation": 0.7853981633974483,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.30322712127489204,
+          "pitch": 0.0377156700954977,
+          "title": "草地",
+          "text": "可以踢球、進行田賽等等"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "觀眾席 北區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1341,
+      "initialViewParameters": {
+        "yaw": -0.016028533946890633,
+        "pitch": -0.015265566047133916,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8998445987554788,
+          "pitch": 0.20352078719323252,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 2.419040276066183,
+          "pitch": 0.331957270058858,
+          "rotation": 0,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5463619105768522,
+          "pitch": -0.10205257889985653,
+          "title": "北區觀眾席",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2--",
+      "name": "觀眾席 西區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1342.5,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8323131069381837,
+          "pitch": 0.18673423700576564,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 2.514780658873847,
+          "pitch": 0.274699234532811,
+          "rotation": 0,
+          "target": "1--"
+        },
+        {
+          "yaw": -2.3277092330004248,
+          "pitch": 0.6058932908199175,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.20645670775030922,
+          "pitch": -0.058869121103303,
+          "title": "西區觀眾席",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3--",
+      "name": "觀眾席 東區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1341,
+      "initialViewParameters": {
+        "yaw": -0.3722181772110318,
+        "pitch": 0.12212452837708909,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5299696230766306,
+          "pitch": 0.3462940207405403,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": -0.830421740714602,
+          "pitch": 0.19159341742735592,
+          "rotation": 0,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.11854328864039587,
+          "pitch": -0.1114349403192918,
+          "title": "東區觀眾席",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4--",
+      "name": "觀眾席 南區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1341.5,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.411793398539042,
+          "pitch": 0.24887283837114893,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -1.0551665218783537,
+          "pitch": 0.30221530514000783,
+          "rotation": 0.7853981633974483,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.43627525468749084,
+          "pitch": -0.10266792633915678,
+          "title": "南區觀眾席",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "台北田徑場",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
